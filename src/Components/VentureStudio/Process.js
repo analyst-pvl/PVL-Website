345b057b0f5857e6454e5e6c721eacbf9410ebdd
analyst-pvl <86413677+analyst-pvl@users.noreply.github.com>
2023-07-13 19:10:27 +0530

    Update Process.js

--- src/Components/VentureStudio/Process.js
+++ src/Components/VentureStudio/Process.js
@@ -43,27 +43,27 @@ const Process = () => {
         <div className="mt-10 md:mt-5 w-full max-w-[520px] last:border-white mb-24">
           <ProcessDiv
             step="01"
-            heading="Choose an Idea"
-            parah="Come with your own project idea, or pick from one of our identified interesting industry-wide problems, backed by corporates"
+            heading="Bootcamp to discover deep tech product"
+            parah="Come with your own project idea, or participate in our bootcamp identified interesting industry-wide problems, backed by corporates"
             border
           />
           <ProcessDiv
             step="02"
-            heading="Build"
-            parah="Plan & build out your project over the course of the next few months with all the support from us"
+            heading="Turn the idea in to a product"
+            parah="Access to tools, hosting, APIs, data sets, mentors, designers & anything else you may require to take your idea to the next level"
             border
           />
           <ProcessDiv
             step="03"
-            heading="Complete"
-            parah="Finish your project with the right teammates and showcase your newly gained skills on your resume"
+            heading="Get your career a 10x jump"
+            parah="Sell your project, convert in to startup or just cash-in the prize money and move on with your life"
             border
           />
-          <ProcessDiv
+         /* <ProcessDiv
             step="04"
             heading="Exit & Iterate"
             parah="Generate value in 1 of 3 ways with your project: Get Acquired and Paid by Us; Create your Startup; Go for External Funding"
-          />
+          /> */
         </div>
       </div>
     </div>
