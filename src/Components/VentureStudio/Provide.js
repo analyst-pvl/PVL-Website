c8cd03c3dceea272d00137376e7d610de9bee288
Mr.Nayra <aryanedu5786@gmail.com>
2023-07-17 11:11:00 +0530

    mobile changes -2

--- src/Components/VentureStudio/Provide.js
+++ src/Components/VentureStudio/Provide.js
@@ -46,7 +46,7 @@ const Provide = () => {
             <HandHolding />
           </Card>
         </div>
-        <h5 className="text-[#060919] text-5xl lg:text-3xl font-bold text-center mt-14">
+        <h5 className="text-[#060919] text-xl lg:text-3xl font-bold text-center mt-14">
           Driven to complete your project, without anxiety on commitments / risks
         </h5>
       </div>
